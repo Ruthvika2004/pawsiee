5f9028dfd13a54131a844289e2b3cd7e1072a9dd
Ruthvika2004 <147825236+Ruthvika2004@users.noreply.github.com>
2024-06-06 11:30:49 +0530

    added counter animation

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function moveProfiles() {
 }
 
 setInterval(moveProfiles, 1000);
+
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const counters = document.querySelectorAll('.counter');
+  
+  counters.forEach(counter => {
+    counter.innerText = '0';
+    
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const current = +counter.innerText;
+      
+      const increment = target / 200;
+      
+      if (current < target) {
+        counter.innerText = `${Math.ceil(current + increment)}`;
+        setTimeout(updateCounter, 10);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    
+    updateCounter();
+  });
+});
